58062b1b75ca0851cce1bd1f4e4b8e6267db4a87
Joan Oliete <joan.oliete@example.com>
2026-05-07 19:48:20 +0200

    chore: bump version to 2.2.0 and update translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import ca from './locales/ca.json';
 import es from './locales/es.json';
 import en from './locales/en.json';
 
 i18n
-    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources: {
@@ -16,13 +14,15 @@ i18n
             en: { translation: en },
         },
         fallbackLng: 'ca',
+        lng: localStorage.getItem('i18nextLng') || 'ca',
         interpolation: {
             escapeValue: false, // React already safe from XSS
-        },
-        detection: {
-            order: ['localStorage'],
-            caches: ['localStorage'],
-        },
+        }
     });
+
+// Make sure any change is saved to localStorage
+i18n.on('languageChanged', (lng) => {
+    localStorage.setItem('i18nextLng', lng);
+});
 
 export default i18n;
